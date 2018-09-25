4dc4352004aef92e336fa094a3b6faafe91d2d2c
heyouli <julia@julia-hofmann.com>
2018-09-26 00:16:29 +0200

    6. Created landing page, copied files over from previous app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
+  get 'simple_pages/about'
   get 'simple_pages/posts'
   get 'simple_pages/hireme'
-  get 'simple_pages/about'
-  get 'simple_pages/contact'
-  root 'simple_pages#index'
+  get 'simple_pages/index'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
