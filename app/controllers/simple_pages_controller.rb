6fb1b702a8ed5b69863dfd4e9c5e2166cb989af1
heyouli <julia@julia-hofmann.com>
2018-10-02 15:48:06 +0200

    set controllers and view for design, photography and recipes

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,4 +3,16 @@ class SimplePagesController < ApplicationController
     @featured_post = Post.first
     @posts = Post.all
   end
+
+  def design
+    @design_post=Post.where(theme: "Design")
+  end
+
+  def photography
+    @photography_post=Post.where(theme: "Photography")
+  end
+
+  def recipes
+    @recipe_post=Post.where(theme: "Recipes")
+  end
 end
